659d2096c97488704aaa128b170924c40738244a
Cyle Gage <cylegage@gmail.com>
2016-12-05 00:32:34 -0500

    improve the ONP cli client

--- onp/client.js
+++ onp/client.js
@@ -1,33 +1,47 @@
-const tls = require('tls');
-const fs = require('fs');
+/**
+ * A simple command-line ONP client.
+ *
+ * Takes at least one argument, being what to look up.
+ * Usage: node client.js "right okay 👌"
+ *
+ * By default, this will look up the address via localhost.
+ * To specify an ONP server to use, use the next argument:
+ * Usage: node client.js "right okay 👌" 192.168.1.1
+ *
+ * By default, this will look up on the default ONP port.
+ * To specify which port to use, use the next argument:
+ * Usage: node client.js "right okay 👌" 192.168.1.1 420
+ */
 
-if (process.argv[2] == undefined) {
+// load the onp module
+const onp = require('./module.js');
+
+// make sure we have a string to even look up
+if (process.argv[2] === undefined) {
     console.log('no request given');
     process.exit(1);
-} else {
-    var ask_for = process.argv[2];
 }
 
-const onp_server_port = 21335;
+// here's the string that we're asking for
+const ask_for = process.argv[2];
 
-const client_options = {
-    rejectUnauthorized: false
-};
+// check to see if the CLI provided a specific ONP service IP to use
+const onp_server =
+    (process.argv[3] !== undefined && process.argv[3].trim() !== '') ?
+        process.argv[3].trim() :
+        undefined;
 
-var tls_stream = tls.connect(onp_server_port, client_options, function() {
-    console.log('client connected');
-    console.log('connection is', tls_stream.authorized ? 'authorized' : 'unauthorized');
-    console.log('request: ' + ask_for);
-    tls_stream.write(ask_for + '\n');
-});
+// check to see if the CLI provided a specific ONP port to use
+const onp_server_port =
+    (process.argv[4] !== undefined && process.argv[4].trim() !== '') ?
+        process.argv[4].trim() :
+        undefined;
 
-tls_stream.setEncoding('utf8');
+// ok, good to try now
+console.log('looking up: ' + ask_for);
 
-tls_stream.on('data', function(data) {
-    console.log('got back: ' + data.toString().trim());
-    process.exit(0);
-});
-
-tls_stream.on('end', function() {
-    console.log('client disconnected');
-});
+// try looking up what IP matches
+onp.getRecord(ask_for, function(record) {
+    // spit out the record info
+    console.log('looked up "' + ask_for + '", result: ' + record);
+}, onp_server, onp_server_port);
